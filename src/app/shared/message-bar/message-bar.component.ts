7d3211c831b31d6d9dddfad2e79640a36db8bc0f
MichalNowosad <michalnowosad96@gmail.com>
2025-07-14 20:36:58 +0200

    Add change detection on push

--- src/app/shared/message-bar/message-bar.component.ts
+++ src/app/shared/message-bar/message-bar.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, input} from '@angular/core';
 
 @Component({
   selector: 'app-message-bar',
   imports: [],
   templateUrl: './message-bar.component.html',
-  styleUrl: './message-bar.component.scss'
+  styleUrl: './message-bar.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MessageBarComponent {
-
 }
